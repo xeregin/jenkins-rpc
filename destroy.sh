9a4294964106f008d2a5db6e41acc1b8293f5c55
Evan Callicoat <apsu@propter.net>
2014-08-11 18:41:23 -0500

    Drop initial tweak from dockerlab

---
+++ destroy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+#[[ -d /opt/virtlab ]] && pushd /opt/virtlab
+#source .venv/bin/activate
+export PYTHONUNBUFFERED=1
+export ANSIBLE_FORCE_COLOR=1
+ansible-playbook destroy.yml $@ 2>&1
+#[[ -d /opt/virtlab ]] && popd
