aeddb2e4ca7bd70eee540fe48f666aeabe3c907f
Evan Callicoat <apsu@propter.net>
2014-08-12 19:33:13 -0500

    Update job.sh since we're using Ansible on target

--- dockerfiles/jenkins-job/job.sh
+++ dockerfiles/jenkins-job/job.sh
@@ -3,10 +3,6 @@
 set -e
 set -x
 
-# Set ansible tweaks
-export PYTHONUNBUFFERED=1
-export ANSIBLE_FORCE_COLOR=1
-
 # Set HOME to /root
 export HOME=/root
 
@@ -23,18 +19,7 @@ trap cleanup 0
 # Clone jenkins-rpc repo
 git clone git@github.com:rcbops/jenkins-rpc.git || true
 
-# Clone ansible-lx-rpc repo
-git clone git@github.com:rcbops/ansible-lxc-rpc.git || true
-
-# Install ansible and other RPC deps
-pip install -r ansible-lxc-rpc/requirements.txt
-
 # Fire up jenkins-rpc
 pushd jenkins-rpc
 ./deploy.sh
-popd
-
-# Fire up RPC
-pushd ansible-lxc-rpc/rpc_deployment
-ansible-playbook -e @vars/user_variables.yml playbooks/all-the-all-the-things.yml
 popd
