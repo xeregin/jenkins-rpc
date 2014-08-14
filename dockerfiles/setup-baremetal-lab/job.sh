28cc51692d0a8c0568a43dfabf80ecce22f80b59
Jacob Wagner <swagner1104@gmail.com>
2014-08-14 15:37:49 -0500

    added setup-baremetal-lab dockerfile

--- dockerfiles/setup-baremetal-lab/job.sh
+++ dockerfiles/setup-baremetal-lab/job.sh
@@ -19,5 +19,19 @@ trap cleanup TERM
 
 # Job commands follow
 
+# Clone jenkins-rpc repo
+git clone git@github.com:rcbops/jenkins-rpc.git & wait || true
+
+# Fire up jenkins-rpc
+pushd jenkins-rpc
+
+# Populate playbook files for SSH keys on targets
+cp ~/.ssh/id_* roles/configure-hosts/files/
+./scripts/deploy.sh & wait
+popd
+
+# Skip deployment and trigger handler
+[[ $BUILD_SKIP == "yes" ]] && cleanup
+
 # Add '& wait' to every long-running job command
 # Example: ansible-playbook blah & wait
