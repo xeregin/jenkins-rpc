c68140fc061853ad088aacb28e5e8a968782e7ca
Jacob Wagner <swagner1104@gmail.com>
2014-08-15 12:38:40 -0500

    added all job place holders for bare metal

---
+++ dockerfiles/build-baremetal-lab/job.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+# Set HOME to /root
+export HOME=/root
+
+# Switch to home directory
+cd
+
+# Cleanup function
+cleanup() {
+  # Disable trap
+  trap - INT TERM EXIT
+  # Kill process group to catch jobs
+  kill 0
+  # Exit if we're keeping the build
+  [[ $BUILD_KEEP == "yes" ]] && exit
+  # Destroy cluster hosts
+  pushd ~/jenkins-rpc
+  ./scripts/destroy.sh
+}
+trap cleanup INT TERM EXIT
+
+# Clone jenkins-rpc repo
+git clone git@github.com:rcbops/jenkins-rpc.git & wait || true
+
+# Fire up jenkins-rpc
+pushd jenkins-rpc
+
+# Populate playbook files for SSH keys on targets
+cp ~/.ssh/id_* roles/configure-hosts/files/
+./scripts/deploy.sh & wait
+popd
+
+# Skip deployment and trigger handler
+[[ $BUILD_SKIP == "yes" ]] && cleanup
+
+# Connect to target and run script
+ssh $(<target.ip) ./target.sh & wait
