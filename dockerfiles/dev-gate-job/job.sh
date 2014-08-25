013cc9c7c46e01c700e751a6cd5521b50fa4122c
Evan Callicoat <apsu@loki.local>
2014-08-24 22:33:40 -0500

    Add dev-gate-job and test host templating

---
+++ dockerfiles/dev-gate-job/job.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+# Set HOME to /root
+export HOME=/root
+
+# Switch to home directory
+cd
+
+# Cleanup function
+cleanup() {
+  # Disable trap
+  trap - INT TERM EXIT
+  # Kill process group to catch jobs
+  kill 0
+  # Exit if we're keeping the build
+  [[ $BUILD_KEEP == "yes" ]] && exit
+}
+trap cleanup INT TERM EXIT
+
+# Clone jenkins-rpc repo
+git clone git@github.com:rcbops/jenkins-rpc.git & wait || true
+
+# Fire up jenkins-rpc
+pushd jenkins-rpc
+
+ansible-playbook -e hosts=${EXECUTOR_NUMBER} playbooks/dev-labs/networking.yml & wait
+popd
+
+# Skip deployment and trigger handler
+[[ $BUILD_SKIP == "yes" ]] && cleanup
+
+# Connect to target and run script
+ssh $(<target.ip) ./target.sh & wait
