f52dec60ae3ac0ce47f26a0ffd5ed5fed86bba75
Evan Callicoat <apsu@loki.local>
2014-08-25 00:54:09 -0500

    Update error handling for proper jenkins statuses

--- dockerfiles/dev-gate-job/job.sh
+++ dockerfiles/dev-gate-job/job.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-set -e
 set -x
 
 # Set HOME to /root
@@ -11,32 +10,35 @@ cd
 
 # Cleanup function
 cleanup() {
-  # Store return value
-  retval=$?
+  retval=${1:-$?}
   # Disable trap
-  trap - INT TERM EXIT
-  # Kill process group to catch jobs
-  kill 0
+  trap - INT TERM
+  # Kill jobs
+  kill $(jobs -p)
   # Exit if we're keeping the build
   [[ $BUILD_KEEP == "yes" ]] && exit $retval
   # Otherwise cleanup
+  # ...
   # Exit
   exit $retval
 }
-trap cleanup INT TERM EXIT
+trap cleanup INT TERM
 
 # Clone jenkins-rpc repo
-git clone -b dev-sat6 git@github.com:Apsu/jenkins-rpc.git & wait || true
+git clone -b dev-sat6 git@github.com:Apsu/jenkins-rpc.git & wait
+[[ $? -ne 0 ]] && cleanup
 
 # Fire up jenkins-rpc
 pushd jenkins-rpc
 export PYTHONUNBUFFERED=1
 export ANSIBLE_FORCE_COLOR=1
 ansible-playbook -i inventory/dev-sat6-lab01 -e hosts=cluster${EXECUTOR_NUMBER} playbooks/dev-labs/site.yml & wait
+[[ $? -ne 0 ]] && cleanup
 popd
 
 # Skip deployment and trigger handler
-[[ $BUILD_SKIP == "yes" ]] && cleanup
+[[ $BUILD_SKIP == "yes" ]] && cleanup 0
 
 # Connect to target and run script
 ssh $(<target.ip) ./target.sh & wait
+cleanup $?
