d7522a417ae4f1f339def727d94d40c2b96d5a31
Hugh Saunders <hugh@wherenow.org>
2014-10-27 16:50:06 +0000

    Reorganise dev-gate-job logic
    move jenkins-rpc playbook and target.sh run under one conditional.

--- dockerfiles/dev-gate-job/job.sh
+++ dockerfiles/dev-gate-job/job.sh
@@ -54,20 +54,21 @@ set_trap cleanup INT TERM ERR
 # Clone jenkins-rpc repo
 git clone git@github.com:rcbops/jenkins-rpc.git & wait %1
 
-# Move into jenkins-rpc
-pushd jenkins-rpc
 
-# Run the lab prep playbook
-export PYTHONUNBUFFERED=1
-export ANSIBLE_FORCE_COLOR=1
-[[ $BUILD == "yes" ]] && ansible-playbook -i inventory/dev-sat6-lab01 -e hosts=cluster${EXECUTOR_NUMBER} -e pullRequestID=${ghprbPullId} playbooks/dev-labs/site.yml & wait %1
-popd
+if [[ $BUILD == "yes" ]]
+then
+  # Move into jenkins-rpc
+  pushd jenkins-rpc
 
-# Skip deployment and trigger success handler
-[[ $BUILD != "yes" ]] && cleanup 0
+  # Prepare the lab
+  export PYTHONUNBUFFERED=1
+  export ANSIBLE_FORCE_COLOR=1
+  ansible-playbook -i inventory/dev-sat6-lab01 -e hosts=cluster${EXECUTOR_NUMBER} -e pullRequestID=${ghprbPullId} playbooks/dev-labs/site.yml & wait %1
 
-# Connect to target and run script - this will kickoff ansible-lxc-rpc
-ssh $(<target.ip) ./target.sh & wait %1
-# Trigger success handler
+  popd
+
+  # run ansible-lxc-rpc
+  ssh $(<target.ip) ./target.sh & wait %1
+fi
 
 cleanup 0
