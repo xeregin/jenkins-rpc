39eb2c36a3300af3d98aca4021e3c57c13a99af0
Hugh Saunders <hugh@wherenow.org>
2014-11-19 13:14:38 +0000

    Continue when failures are hit
    Failure is recorded so the job will definitely fail, but the job will continue.
    This could be useful, as the failure may be unrelated (eg utility
    container start fail).

--- roles/dev-setup-rpc/files/target.sh
+++ roles/dev-setup-rpc/files/target.sh
@@ -7,8 +7,20 @@ export PYTHONUNBUFFERED=1
 export ANSIBLE_FORCE_COLOR=1
 
 pushd ansible-lxc-rpc/rpc_deployment
-ansible-playbook -e @/root/rpc_deploy/user_variables.yml playbooks/infrastructure/haproxy-install.yml
-ansible-playbook -e @/root/rpc_deploy/user_variables.yml playbooks/setup-everything.yml
-ansible-playbook -e @/root/rpc_deploy/user_variables.yml playbooks/openstack/tempest.yml
+(
+  set +e
+  rc=0
+  ansible-playbook -e @/root/rpc_deploy/user_variables.yml playbooks/infrastructure/haproxy-install.yml
+  rc=$(($rc + $?))
+  ansible-playbook -e @/root/rpc_deploy/user_variables.yml playbooks/setup-everything.yml
+  rc=$(($rc + $?))
+  ansible-playbook -e @/root/rpc_deploy/user_variables.yml playbooks/openstack/tempest.yml
+  rc=$(($rc + $?))
+  exit $rc
+)
+ansible_result=$?
 ansible "utility_all[0]" -m shell -a /root/rpc_tempest_gate.sh
 popd
+
+#will have already exited failure if rpc_tempest_gate.sh failed, due to -e.
+exit $ansible_result
