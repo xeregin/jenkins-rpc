8250c9bb23ec950fdc2ebd74cfc9baedc9902134
Hugh Saunders <hugh@wherenow.org>
2014-11-20 14:05:16 +0000

    Always run tempest (2)
    Fix previous error where subshell fail would have caused the parent
    shell to exit due to -e.
    
    Instead, collect return codes into rc and exit with that.

--- roles/dev-setup-rpc/files/target.sh
+++ roles/dev-setup-rpc/files/target.sh
@@ -1,26 +1,21 @@
 #!/usr/bin/env bash
-
-set -e
 set -x
 
 export PYTHONUNBUFFERED=1
 export ANSIBLE_FORCE_COLOR=1
 
+#Add Return Code
+arc(){
+  rc=$(($rc + $?))
+}
+
+rc=0
+
 pushd ansible-lxc-rpc/rpc_deployment
-(
-  set +e
-  rc=0
-  ansible-playbook -e @/root/rpc_deploy/user_variables.yml playbooks/infrastructure/haproxy-install.yml
-  rc=$(($rc + $?))
-  ansible-playbook -e @/root/rpc_deploy/user_variables.yml playbooks/setup-everything.yml
-  rc=$(($rc + $?))
-  ansible-playbook -e @/root/rpc_deploy/user_variables.yml playbooks/openstack/tempest.yml
-  rc=$(($rc + $?))
-  exit $rc
-)
-ansible_result=$?
-ansible "utility_all[0]" -m shell -a /root/rpc_tempest_gate.sh
+ansible-playbook -e @/root/rpc_deploy/user_variables.yml playbooks/infrastructure/haproxy-install.yml; arc
+ansible-playbook -e @/root/rpc_deploy/user_variables.yml playbooks/setup-everything.yml; arc
+ansible-playbook -e @/root/rpc_deploy/user_variables.yml playbooks/openstack/tempest.yml; arc
+ansible "utility_all[0]" -m shell -a /root/rpc_tempest_gate.sh; arc
 popd
 
-#will have already exited failure if rpc_tempest_gate.sh failed, due to -e.
-exit $ansible_result
+exit $rc
