8614eb34067e8fb66ada4e623a4f3df22a8d16cc
Andy McCrae <andy.mccrae@gmail.com>
2014-12-02 13:25:04 +0000

    Add initial swift support
    * Adjust the rpc_user_config to include swift sections
    * Add a dev-swift role to setup swift lvs and mount them
    * Add swift-all.yml to the playbooks that are run
    
    Fixes #80

--- roles/dev-setup-rpc/files/target.sh
+++ roles/dev-setup-rpc/files/target.sh
@@ -15,6 +15,7 @@ rc=0
 pushd ansible-lxc-rpc/rpc_deployment
 ansible-playbook -e @/root/rpc_deploy/user_variables.yml playbooks/infrastructure/haproxy-install.yml; arc
 ansible-playbook -e @/root/rpc_deploy/user_variables.yml playbooks/setup-everything.yml; arc
+ansible-playbook -e @/root/rpc_deploy/user_variables.yml playbooks/openstack/swift-all.yml; arc
 ansible-playbook -e @/root/rpc_deploy/user_variables.yml playbooks/openstack/tempest.yml; arc
 ansible "utility_all[0]" -m shell -a /root/rpc_tempest_gate.sh; arc
 popd
