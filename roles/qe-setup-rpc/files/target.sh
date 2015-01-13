7713ebc1d19a1c7fcfb27e7bd1fa73d660aa3175
Julian Montez <julian.montez@gmail.com>
2015-01-13 17:28:46 -0600

    Move 'rpc_deploy' to home directory

--- roles/qe-setup-rpc/files/target.sh
+++ roles/qe-setup-rpc/files/target.sh
@@ -8,7 +8,7 @@ export PYTHONUNBUFFERED=1
 export ANSIBLE_FORCE_COLOR=1
 
 pushd ansible-lxc-rpc/rpc_deployment
-ansible-playbook -e @/etc/rpc_deploy/user_variables.yml playbooks/setup/host-setup.yml
-ansible-playbook -e @/etc/rpc_deploy/user_variables.yml playbooks/infrastructure/infrastructure-setup.yml
-ansible-playbook -e @/etc/rpc_deploy/user_variables.yml playbooks/openstack/openstack-setup.yml
+ansible-playbook -e @$HOME/rpc_deploy/user_variables.yml playbooks/setup/host-setup.yml
+ansible-playbook -e @$HOME/rpc_deploy/user_variables.yml playbooks/infrastructure/infrastructure-setup.yml
+ansible-playbook -e @$HOME/rpc_deploy/user_variables.yml playbooks/openstack/openstack-setup.yml
 popd
