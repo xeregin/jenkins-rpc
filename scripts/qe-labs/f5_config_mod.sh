6e18d188f800b25867f80dccfcfe5f0b6edc5df5
Ian Logan <xeregin@gmail.com>
2015-01-19 13:31:08 -0600

    Adding f5 config mod script

---
+++ scripts/qe-labs/f5_config_mod.sh
@@ -0,0 +1,13 @@
+if [[ $1 == "" || $2 == "" ]]; then
+    echo "Expected usage: $0 <config_file> <IP_mod>"
+    exit
+fi
+
+if [[ `stat $1 | sed '/stdin/d' 2> /dev/null` ]]; then
+    echo "Config File found! Continuing..."
+else
+    echo "Could not find config file ( $1 )! Terminating..."
+    exit
+fi
+
+cat $1 | sed "s/\([0-9]*\.[0-9]*\.[0-9]*\.[0-9]*[:]*[0-9]*\)/\1%$2/" > MOD_$1
