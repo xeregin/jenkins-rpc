b30d40f2ac40febe1f519b2f0580fe2de45fb279
Jacob Wagner <swagner1104@gmail.com>
2015-03-11 14:30:15 -0500

    added ability to test the latest tag for each release, fixes #163
    (cherry picked from commit 4acb4c4842b3c550534b26f290d7e3fe8c62fa16)

--- dockerfiles/nightly-job/job.sh
+++ dockerfiles/nightly-job/job.sh
@@ -44,7 +44,7 @@ cleanup() {
 
   # Rekick the nodes in preperation for the next run.
   #[ -e playbooks ] || pushd jenkins-rpc
-  #git checkout $targetBranch
+  #git checkout $RELEASE
   #[[ $REKICK == "yes" ]] &&  ansible-playbook -i playbooks/inventory/$LAB -e @playbook/vars/$LAB playbooks/rekick-lab.yml ||:
 
   # Exit
@@ -57,11 +57,11 @@ set_trap cleanup INT TERM ERR
 # Clone jenkins-rpc repo
 git clone git@github.com:rcbops/jenkins-rpc.git & wait %1
 
-if [[ $BUILD == "yes" ]]
+if [[ $BUILD == "branch" ]]
 then
   # Move into jenkins-rpc
   pushd jenkins-rpc
-  git checkout $targetBranch
+  git checkout $RELEASE
  
   # Set color and buffer
   export PYTHONUNBUFFERED=1
@@ -71,8 +71,26 @@ then
   ansible-playbook \
     -i playbooks/inventory/$LAB \
     -e @playbooks/vars/$LAB \
-    -e @playbooks/vars/branch-vars-$targetBranch \
+    -e @playbooks/vars/branch-vars-$RELEASE \
     playbooks/nightly-multinode.yml & wait %1
+elif [[ $BUILD == "tag" ]]
+then
+  # Move into jenkins-rpc
+  pushd jenkins-rpc
+  git checkout $RELEASE
+ 
+  # Set color and buffer
+  export PYTHONUNBUFFERED=1
+  export ANSIBLE_FORCE_COLOR=1
+
+  # Preconfigure lab / build RPC / test RPC
+  ansible-playbook \
+    -i playbooks/inventory/$LAB \
+    -e @playbooks/vars/$LAB \
+    -e @playbooks/vars/branch-vars-$RELEASE \
+    -e LATEST_TAG=true
+    playbooks/nightly-multinode.yml & wait %1
+
 fi
 
 # Exit cleanly
