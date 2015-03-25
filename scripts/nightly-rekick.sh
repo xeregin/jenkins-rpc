cf7ba0d02ce26270a9fcb1ca49110209396f3b93
Jacob Wagner <swagner1104@gmail.com>
2015-03-25 11:42:21 -0500

    fixed playbook calling issues in scripts, renamed rekick for nightly
    (cherry picked from commit badd52005e75598d4cf09a279bd10da18fd3036a)

--- scripts/nightly-rekick.sh
+++ scripts/nightly-rekick.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 LAB=${LAB:-master}
-TAGS=${TAGS:-prepare,run,test}
-OS_ANSIBLE_BRANCH=${OS_ANSIBLE_BRANCH:-master}
 ANSIBLE_OPTIONS=${ANSIBLE_OPTIONS:--v}
 
 ansible-playbook \
   -i inventory/nighty-${LAB} \
   -e @vars/nightly-${LAB}.yml \
-  -e os_ansible_branch=${OS_ANSIBLE_BRANCH} \
-  --tags $TAGS \
   $ANSIBLE_OPTIONS \
-  nightly-multinode.yml
+  nightly-rekick.yml
