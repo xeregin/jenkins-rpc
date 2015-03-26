7f206929e041d266451c4568ea09c7cec230219b
Jacob Wagner <swagner1104@gmail.com>
2015-03-26 11:59:08 -0500

    massive redo of how we run os-ansible scripts due to redirection ansible issues
    (cherry picked from commit b85d16b6d43004f891a0b491318b68140d95e4c6)
    
    Conflicts:
    	playbooks/vars/nightly-kilo.yml
    	playbooks/vars/nightly-master.yml
    	scripts/nightly-multinode.sh
    	scripts/nightly-rekick.sh

--- scripts/nightly-multinode.sh
+++ scripts/nightly-multinode.sh
@@ -1,13 +1,79 @@
 #!/usr/bin/env bash
+
+### -------------- [ Variables ] --------------------
 LAB=${LAB:-master}
-TAGS=${TAGS:-prepare,run,test}
+TAGS=${TAGS:-rekick,prepare,run,test}
+OS_ANSIBLE_URL=${OS_ANSIBLE_URL:-git@github.com:stackforge/os-ansible-deployment.git}
 OS_ANSIBLE_BRANCH=${OS_ANSIBLE_BRANCH:-master}
+JENKINS_RPC_URL=${JENKINS_RPC_URL:-git@github.com:rcbops/jenkins-rpc.git}
+JENKINS_RPC_BRANCH=${JENKINS_RPC_BRANCH:-master}
+TEMPEST_SCRIPT_PARAMETERS=${TEMPEST_SCRIPT_PARAMETERS:-scenario}
+ANSIBLE_FORCE_COLOR=${ANSIBLE_FORCE_COLOR:-1}
 ANSIBLE_OPTIONS=${ANSIBLE_OPTIONS:--v}
 
-ansible-playbook \
-  -i inventory/nightly-${LAB} \
-  -e @vars/nightly-${LAB}.yml \
-  -e os_ansible_branch=${OS_ANSIBLE_BRANCH} \
-  --tags $TAGS \
-  $ANSIBLE_OPTIONS \
-  nightly-multinode.yml
+### -------------- [ Functions ] --------------------
+
+env
+
+run_playbook_tag(){
+  export ANSIBLE_FORCE_COLOR
+  echo "Running tag ${1} from jenkins-rpc/nightly-multinode.yml"
+  ansible-playbook \
+    -i inventory/nightly-${LAB}\
+    -e @vars/nightly-${LAB}.yml\
+    -e os_ansible_branch=${OS_ANSIBLE_BRANCH}\
+    --tags $1\
+    $ANSIBLE_OPTIONS\
+    nightly-multinode.yml
+}
+
+run_script(){
+  #Find the first node ip from the inventory
+  [[ -z $infra_1_ip ]] && infra_1_ip=$(grep -o -m 1 '10.127.[0-9]\+.[0-9]\+' \
+                          < inventory/nightly-$LAB)
+  : >> /tmp/env
+  echo "export ANSIBLE_FORCE_COLOR=$ANSIBLE_FORCE_COLOR" >> script_env
+  scp script_env $infra_1_ip:/tmp/env
+  echo "Running script ${1} from os-ansible-deployment/scripts."
+  ssh root@$infra_1_ip ". /tmp/env; cd ~/rpc_repo; bash scripts/${1}.sh"
+}
+
+prepare(){
+  run_playbook_tag prepare
+  run_script bootstrap-ansible
+  run_playbook_tag configure
+  run_playbook_tag reboot
+  # sleep for 2 minutes to wait for ssh
+  echo "Sleeping for 2 minutes to allow ssh to come up."
+  sleep 120
+}
+
+run(){
+  echo "export DEPLOY_TEMPEST=yes" > script_env
+  run_script run-playbooks
+}
+
+test(){
+  echo "export TEMPEST_SCRIPT_PARAMETERS=${TEMPEST_SCRIPT_PARAMETERS}" > script_env
+  run_script run-tempest
+}
+
+rekick(){
+  run_playbook_tag rekick
+  # sleep for 3 minutes to wait for ssh
+  echo "Sleeping for 3 minutes to allow ssh to come up."
+  sleep 180
+}
+
+### -------------- [ Main ] --------------------
+
+# run the tags that are required until something breaks
+rc=0
+for tag in ${TAGS}
+do
+  $tag
+  rc=$(( $rc + $? ))
+  [[ $rc -ne 0 ]] && break
+done
+
+exit $rc
