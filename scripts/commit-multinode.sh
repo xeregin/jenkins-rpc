fe00fc131203222d8ea740fbeaad9167e3ccc81a
Hugh Saunders <hugh@wherenow.org>
2015-04-16 14:02:59 +0100

    Add branch checkout to upgrade function Upgrade script doesn't switch branches so have to do that first
    Also: Remove Curl for upgrade script as its now in tree

--- scripts/commit-multinode.sh
+++ scripts/commit-multinode.sh
@@ -132,7 +132,9 @@ release(){
 }
 
 upgrade(){
-  ssh_command "curl $UPGRADE_SCRIPT_URL >~/rpc_repo/scripts/upgrade_script.sh; cd ~/rpc_repo; echo YES | bash scripts/upgrade_script.sh"
+  ssh_command "cd ~/rpc_repo;
+               git checkout $UPGRADE_BRANCH;
+               echo YES | bash scripts/upgrade_script.sh"
 }
 
 # A propterties file (Java key=value format) is produced to be read by the
