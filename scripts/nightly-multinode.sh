4bb769bd23a305a4fcbff1170091cc90ccd312b4
Jacob Wagner <swagner1104@gmail.com>
2015-05-05 15:55:46 -0500

    removed the reset of OS_ANSIBLE_BRANCH
    (cherry picked from commit 7fc97bae9aabb8366e4ad52a4dfba4c033bde274)

--- scripts/nightly-multinode.sh
+++ scripts/nightly-multinode.sh
@@ -73,9 +73,6 @@ test(){
 }
 
 upgrade(){
-  # set os_ansible_branch environment varible for upgrade
-  OS_ANSIBLE_BRANCH=${UPGRADE_BRANCH}
-
   # run jenkins-rpc upgrade tag
   run_playbook_tag upgrade
 
